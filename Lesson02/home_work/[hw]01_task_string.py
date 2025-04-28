5e87d31ff3cbb639a67cd068c1557d81e0846df2
boo <booblegum42@gmail.com>
2025-04-28 15:54:54 +0700

    Lesson02-3 homework complete

--- Lesson02/home_work/[hw]01_task_string.py
+++ Lesson02/home_work/[hw]01_task_string.py
@@ -1,4 +1,15 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
 
-text = ...
+text = ",Произвольный, текст.."
+
+num_comma = text.count(",")
+num_points = text.count(".")
+
+if num_comma > num_points:
+    print(",")
+elif num_comma < num_points:
+    print(".")
+else:
+    print("одинаково")
+
--- Lesson02/home_work/[hw]03_task_string.py
+++ Lesson02/home_work/[hw]03_task_string.py
@@ -1,3 +1,16 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+#       012345678910
+text = "Simple teyy".upper()
+letters = "BCDFGHJKLMNPQRSTVWXZ"
+
+index = 0
+num_consonants = 0
+
+
+while index < len(text):
+    if text[index] in letters:
+        num_consonants += 1
+    index += 1
+
+print(num_consonants)
