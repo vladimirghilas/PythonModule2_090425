ebdecbfce68d73ae8292aa219db282690adf0139
boo <booblegum42@gmail.com>
2025-05-06 20:46:22 +0700

    Lesson10 Project Shop part 1

--- Lesson10/practice/Project_Shop/project_shop.py
+++ Lesson10/practice/Project_Shop/project_shop.py
@@ -11,14 +11,37 @@ def display_menu():
     print("8. Выход. \n")
 
 
+def show_items(items: list[dict]):
+    for i, item in enumerate(items):
+        print(f"{i} Название: {item["name"]}, Цена: {item["price"]}, Количество: {item["quantity"]}")
+
+
+def add_item_to_inventory(items: list[dict]) -> None:
+    """Добавляет товар в инвентарь"""
+    name = input("Введите название товара: ")
+    price = float(input("Введите цену товара: "))
+    quantity = int(input("Введите количество товара: "))
+    new_item = {
+        "name": name,
+        "price": price,
+        "quantity": quantity
+    }
+    items.append(new_item)
+
+
+inventory = [
+    {"name": "Ноутбук", "price": 1200, "quantity": 10},
+    {"name": "Мышь", "price": 25, "quantity": 50},
+    {"name": "Клавиатура", "price": 50, "quantity": 30},
+]
+
 # Menu
 while True:
-    inventory = [
-        {"name": "Ноутбук", "price": 1200, "quantity": 10},
-        {"name": "Мышь", "price": 25, "quantity": 50},
-        {"name": "Клавиатура", "price": 50, "quantity": 30},
-    ]
     display_menu()
     choice = input("Выберите действие: ")
 
-    ...
+    if choice == "1":
+        show_items(inventory)
+        input("Press Enter to continue")
+    elif choice == "2":
+        add_item_to_inventory(inventory)
