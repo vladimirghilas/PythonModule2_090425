6d484e3c50def15b15bb3d12eff42524db2a2d8a
boo <booblegum42@gmail.com>
2025-05-06 15:54:45 +0700

    Lesson10 dict complete

--- Lesson10/practice/01_dict/05_repeat_dict.py
+++ Lesson10/practice/01_dict/05_repeat_dict.py
@@ -6,4 +6,19 @@
 # Вызовите функцию для поиска товара и выведите результат.
 
 def find_item_by_name(items: list[dict], name: str) -> dict | None:
-    pass
+    for item in items:
+        if item["name"].lower() == name.lower():
+            return item
+
+    return None
+
+items = [
+    {"name": "Брюки", "cost": 35, "quantity": 10},
+    {"name": "Кепка", "cost": 5, "quantity": 8},
+    {"name": "кеды", "cost": 8, "quantity": 100},
+    {"name": "Рубашка", "cost": 12, "quantity": 16},
+]
+
+print(find_item_by_name(items, "Брюки"))
+print(find_item_by_name(items, "Носки"))
+print(find_item_by_name(items, "кепка"))
