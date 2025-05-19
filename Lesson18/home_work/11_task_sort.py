d308094fb36d6291f67d646e063e431fa861bb24
Vladimir Ghilas <vladghil37@gmail.com>
2025-05-20 02:45:19 +0300

    Update 11_task_sort.py

--- Lesson18/home_work/11_task_sort.py
+++ Lesson18/home_work/11_task_sort.py
@@ -1,2 +1,13 @@
 # Анаграммы*
 # Дан список слов. Найти в нем все анаграммы (слова, составленные из одних и тех же букв).
+from collections import Counter
+
+words = []
+sorted_words = [''.join(sorted(word)) for word in words]
+
+word_counter = Counter(words)
+duplicate = [word for word, count in word_counter.items() if count > 1]
+
+print(duplicate)
+
+#Vladimir Ghilas
