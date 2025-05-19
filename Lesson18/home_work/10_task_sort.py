a79ed31e42c90afcf97ae9ab23a5c5cc66b7aa07
Vladimir Ghilas <vladghil37@gmail.com>
2025-05-20 02:29:48 +0300

    Update 10_task_sort.py

--- Lesson18/home_work/10_task_sort.py
+++ Lesson18/home_work/10_task_sort.py
@@ -3,3 +3,30 @@
 #
 # Необходимо разбить их на две кучи таким образом, чтобы веса куч отличались не более чем в 2 раза.
 # Если этого сделать нельзя, то указать это.
+import random
+
+n = int(input("enter N "))
+
+
+def gen_rand_num(n, start=1, end=1000):
+    return [random.randint(start, end) for _ in range(n)]
+
+
+list_ = gen_rand_num(n)
+list_.sort()
+i = 0
+for i in range(1, n):
+    first = list_[:n - i]
+    second = list_[n - i:]
+    sum_first = sum(first)
+    sum_second = sum(second)
+    if sum_first <= 2 * sum_second and sum_second <= 2 * sum_first:
+        break
+else:
+    print("не подходящий список для задания")
+print(f'first part: {first}')
+print(f'second part: {second}')
+print(f'summa first {sum_first}')
+print(f'summa second {sum_second}')
+
+#Vladimir Ghilas
