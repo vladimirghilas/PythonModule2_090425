0250df2c34e4f796e0d062fcef6b6c54ed4fe018
Vladimir Ghilas <vladghil37@gmail.com>
2025-05-20 21:51:18 +0300

    Update 02_hw_datetime.py

--- Lesson19/home_work/02_hw_datetime.py
+++ Lesson19/home_work/02_hw_datetime.py
@@ -2,12 +2,14 @@
 # Функция должна вернуть True, если date_string соответствует format_string,
 # и False в противном случае.
 # Обработку исключений использовать обязательно.
-
 from datetime import datetime
 
 def is_valid_date_format(date_string: str, format_string: str):
-    # Допишите код здесь
-    return False
+    try:
+        datetime.strptime(date_string,format_string)
+        return True
+    except ValueError:
+        return False
 
 # Пример использования
 date_str1 = "2025-05-10"
@@ -17,3 +19,5 @@ format_str2 = "%Y-%m-%d"
 
 print(f"'{date_str1}' соответствует формату '{format_str1}': {is_valid_date_format(date_str1, format_str1)}")
 print(f"'{date_str2}' соответствует формату '{format_str2}': {is_valid_date_format(date_str2, format_str2)}")
+
+#Vladimir Ghilas
